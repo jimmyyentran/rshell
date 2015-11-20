4b7cc20e37ba5f56a1a30c37a88eb7d8f13f269a
Jimmy Tran <jtran064@ucr.edu>
2015-11-19 18:35:43 -0800

    test pull/push

--- tests/all.sh
+++ tests/all.sh
@@ -2,12 +2,12 @@
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-$(dirname "$BASEDIR")/tests/single_command.sh $1
-$(dirname "$BASEDIR")/tests/multi_command.sh $1
-$(dirname "$BASEDIR")/tests/commented_command.sh $1
-$(dirname "$BASEDIR")/tests/exit.sh $1
-$(dirname "$BASEDIR")/tests/precedence_test.sh $1
-$(dirname "$BASEDIR")/tests/test_command.sh $1
+# $(dirname "$BASEDIR")/tests/single_command.sh $1
+# $(dirname "$BASEDIR")/tests/multi_command.sh $1
+# $(dirname "$BASEDIR")/tests/commented_command.sh $1
+# $(dirname "$BASEDIR")/tests/exit.sh $1
+# $(dirname "$BASEDIR")/tests/precedence_test.sh $1
+# $(dirname "$BASEDIR")/tests/test_command.sh $1
 $(dirname "$BASEDIR")/tests/bracket_command.sh $1
 
 
